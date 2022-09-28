ad07f66f5431de5215e0efc72e2cecb2572f8bd3
chenshuaiyuan <chenshuaiyuan@wxvrgvtech.com>
2022-09-28 17:17:55 +0800

    提交

--- pages/success/success.vue
+++ pages/success/success.vue
@@ -2,12 +2,15 @@
 	<view style="background-color: #fafafa;height: 100vh;">
 		
 		<div style="width: 100%; text-align: center;">
-			<img src="@/static/success.png" style="margin-top: 20px; width: 100px;"/>
+			<img src="@/static/success.png" style="margin-top: 20px; width: 50px;"/>
 		</div>
 		
 		<div style="width: 100%; text-align: center; margin-top: 30px;">
 			<h2 style="color: rgb(5 150 105)">预约成功</h2>
 			<p style="margin: 20px; color: rgb(5 150 105)">我们将尽快安排人员与您联系，请保持电话畅通，再次感谢您的预约。</p>
+		</div>
+		<div  style="width: 100%; text-align: center; margin-top: 10px;">
+			<img src="@/static/wx-fk.jpg" style="margin-top: 20px; width: 70vw;"/>
 		</div>
 	</view>
 </template>
